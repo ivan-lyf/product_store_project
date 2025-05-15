220c8e7974339b7ee0dfdd16a236061bffab9992
Ivan <134543432+ivan-lyf@users.noreply.github.com>
2025-05-14 21:38:41 -0700

    Initial commit

---
+++ back_end/server.js
@@ -0,0 +1,22 @@
+// const express = require('express'); This sintax works but not too modern
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+import productRoute from "./routes/product.routes.js";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json()); // allows to accept JSON data in req.body
+
+app.use("/api/products", productRoute);
+
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server started at http://localhost:" + PORT);
+});
