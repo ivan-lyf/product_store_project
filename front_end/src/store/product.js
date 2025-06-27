0e01540a2c8c9746302bf15d9fe59bc04c1e3689
Ivan <134543432+ivan-lyf@users.noreply.github.com>
2025-06-27 11:24:26 -0700

    fix deploying issue

--- front_end/src/store/product.js
+++ front_end/src/store/product.js
@@ -1,13 +1,17 @@
 import {create} from 'zustand'
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"
+
 export const useProductStore = create((set) => ({
     products: [],
     setProducts: (products) => set({ products }),
+
+
     createProduct: async (newProduct) => {
         if(!newProduct.name || !newProduct.price || !newProduct.image){
             return{success: false, message: 'Please fill in all fields.'}
         }
-        const res = await fetch("/api/products",{
+        const res = await fetch(`${BASE_URL}/api/products`,{
             method: "POST",
             headers:{
                 "Content-Type":"application/json"
@@ -18,13 +22,17 @@ export const useProductStore = create((set) => ({
         set((state) => ({products:[...state.products, data.data]}));
         return{success: true, message: 'Product created.'}
     },
+
+
     fetchProducts: async() => {
-        const res = await fetch("/api/products");
+        const res = await fetch(`${BASE_URL}/api/products`);
         const data = await res.json();
         set({products: data.data});
     },
+
+
     deleteProduct: async (pid) =>{
-        const res = await fetch(`/api/products/${pid}`,{
+        const res = await fetch(`${BASE_URL}/api/products${pid}`,{
             method: "DELETE",
         });
         const data = await res.json();
@@ -33,8 +41,10 @@ export const useProductStore = create((set) => ({
         set(state => ({products: state.products.filter(products => products._id !== pid)}));
         return{success: true, message: data.message};
     },
+
+
     updateProduct: async(pid, updatedProduct) =>{
-        const res = await fetch(`/api/products/${pid}`, {
+        const res = await fetch(`${BASE_URL}/api/products${pid}`, {
             method: "PUT",
             headers: {
                 "Content-Type" : 'application/json',
